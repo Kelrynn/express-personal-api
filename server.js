b229a5ef6d9d99dbc0d9ad40f7d1ee9063a51483
Kelrynn <cameroncastells@gmail.com>
2017-10-11 19:38:07 -0600

    adding PUT and DELETE routes

--- server.js
+++ server.js
@@ -45,13 +45,15 @@ app.get('/api', function api_index(req, res) {
       {method: "GET", path: "/api", description: "Describes all available endpoints"},//done
       {method: "GET", path: "/api/profile", description: "Data about me"},//done
       {method: "GET", path: "/api/pets", description: "GETs all pets"},//done
-      {method: "POST", path: "/api/pets", description: "creates a new pet"},
-      {method: "PUT", path: "/api/pets/:id", description: "Edits a Pet"},
-      {method: "DELETE", path: "/api/pets/:id", description: "creates a new pet"}
+      {method: "GET", path: "/api/pets/:id", description: "GETs one pet, by ID"},//done      
+      {method: "POST", path: "/api/pets", description: "creates a new pet"},//done
+      {method: "PUT", path: "/api/pets/:id", description: "Edits a Pet"},//done
+      {method: "DELETE", path: "/api/pets/:id", description: "creates a new pet"}//done
     ]
   })
 });
 
+// GET Profile JSON (user data)
 app.get('/api/profile', function(req, res) {
   let profile = {
     name: "Cameron Castells",
@@ -65,20 +67,40 @@ app.get('/api/profile', function(req, res) {
     res.json(profile);
   });
 });
-
+//GET all pets in JSON 
 app.get('/api/pets', function(req, res) {
   db.Pets.find({}, function(err, pets){
     if(err) throw err;
     res.json({pets});
   });
 });
-
+//GET one pet by id
+app.get('/api/pets/:id', function(req, res) {
+  db.Pets.findOne({_id: req.params.id}, function(err, pet){
+    if (err) throw err;
+    res.json(pet);
+  })
+})
+//POST a new pet, return a JSON object of the added pet
 app.post('/api/pets', function(req, res) {
   db.Pets.create(req.body, function(err, pet) {
     if(err) throw err;
-    console.log("Pet created");
     res.json(pet);
-  })
+  });
+});
+//PUT a new pet, EDIT the pet by ID.
+app.put('/api/pets/:id', function(req, res){
+  db.Pets.update({_id: req.params.id}, req.body,function(err, data){
+    if (err) throw err;
+    res.json(data);
+  });
+});
+//DELETE a pet
+app.delete("/api/pets/:id", function (req, res) {
+  db.Pets.remove({_id: req.params.id}, function(err, data) {
+    if (err) throw err;
+    res.json(data);
+  });
 });
 
 /**********
